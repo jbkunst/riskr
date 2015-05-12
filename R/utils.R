4324c3ed71032113d994ff91695e255a6237cfd6
Joshua <jbkunst@gmail.com>
2015-05-12 18:02:14 -0300

    uploading sd_df

---
+++ R/utils.R
@@ -0,0 +1,29 @@
+#' Standarize a data frame
+#' @description This function standarize a data frame converting factors to characters, setting to lowercase the column names.
+#' @param df A data frame
+#' @return The same df with \code{tbl_df} class, lower names.
+#' @examples
+#' df <- data.frame(LETTERS, AA = rnorm(length(LETTERS)), leTTers2 = letters)
+#' str(df)
+#' str(sd_df(df))
+#' @export
+sd_df <- function(df, to.lower.df.names = TRUE, factor.to.string = TRUE){
+  
+  library("plyr")
+  library("dplyr")
+  
+  df <- df %>% tbl_df()
+  
+  if (to.lower.df.names) {
+    names(df) <- tolower(names(df))  
+  }
+  
+  if (factor.to.string) {
+    
+    df[,laply(df, is.factor)] <- lapply(df[,laply(df, is.factor)] , as.character)
+  
+  }
+  
+  df
+  
+}
