a884809653f43247bc158a0160823e21a5792674
Joshua <jbkunst@gmail.com>
2015-08-05 18:10:37 -0300

    min changes

--- R/sim_data_models.R
+++ R/sim_data_models.R
@@ -1,7 +1,7 @@
 library("dplyr")
 library("ggplot2")
 library("ggthemes")
-
+update_geom_defaults("line", list(size = 1.2))
 theme_set(theme_fivethirtyeight(base_size = 11) +
             theme(axis.title = element_text(colour = "grey30"),
                   axis.title.y = element_text(angle = 90),
@@ -16,7 +16,7 @@ model <- rbeta(n, 2, 1)
 target <- rbinom(n, 1, prob = model)
 
 other_model <- model + runif(n)
-and_other_model <- ifelse(target < 0.5, model + runif(n) * 0.5,  model + runif(n) * 1.5)
+and_other_model <- ifelse(target < 0.5, model + runif(n),  model + runif(n) * 1.5)
 random_model <- runif(n)
 perfect_model <- ifelse(target < 0.5, runif(n) * 0.49, runif(n) * 0.5 + .51)
 
@@ -25,10 +25,9 @@ models_df <- data.frame(target, model, other_model, and_other_model, random_mode
 head(models_df)
 
 
-update_geom_defaults("line", list(size = 1.2))
+
 
 gg_roc(target, models_df %>% select(contains("model"))) +
-  theme_fivethirtyeight() + 
   scale_color_hc("darkunica") + 
   coord_equal()
 
