f1fb296acfd3844c035ff3ab1e7d7341584f09cf
Joshua <jbkunst@gmail.com>
2015-08-12 16:43:34 -0300

    fix in gb_sm

--- R/gb_sm.R
+++ R/gb_sm.R
@@ -8,16 +8,20 @@ n <- NULL
 #' 
 #' set.seed(1313)
 #' x <- sample(LETTERS[1:4], size = 100, prob = 1:4/4, replace = TRUE)
-#' data <- data.frame(class = x)
+#' X <- sample(letters[1:2], size = 100, prob = 1:2/2, replace = TRUE)
+#' data <- data.frame(class = x, class2 = X)
 #' gb_sm(data, class)
+#' gb_sm(data, class, class2)
 #'
 #' @export
 gb_sm <- function(data, ...){
 
   data %>%
     dplyr::tbl_df() %>%
-    dplyr::count(..., sort = TRUE) %>% 
+    dplyr::count(...) %>% 
+    dplyr::ungroup() %>% 
     dplyr::rename(count = n) %>% 
-    dplyr::mutate(percent = count/sum(count))
+    dplyr::mutate(percent = count/sum(count)) %>% 
+    dplyr::arrange(desc(count))
 
 }
