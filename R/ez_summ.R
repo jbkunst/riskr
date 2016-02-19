a8c530643c6a1fa318466202b4635d9c15cc6b29
Joshua <jbkunst@gmail.com>
2016-02-19 11:47:36 -0300

    ez_summaries ready

--- R/ez_summ.R
+++ R/ez_summ.R
@@ -40,34 +40,82 @@ select_numeric <- function(df){
   
 }
 
-#' #' Generate Tables for categorical variables
-#' #' 
-#' #' @param df A data frame.
-#' #' 
-#' #' @examples 
-#' #' 
-#' #' data("credit")
-#' #' 
-#' #' credit %>% select_categorical() %>% ez_summ_cat()
-#' #' 
-#' #' @export
-#' ez_summ_cat <- function(df){
-#'   
-#'   key <- NULL
-#'   
-#'   grp_cols <- names(attr(df, "labels"))
-#'   
-#'   df %>%
-#'     purrr::map_if(is.factor, as.character) %>% # avoid warning
-#'     as_data_frame() %>% # this ungroup the df
-#'     group_by_(.dots = lapply(grp_cols, as.symbol)) %>%  # http://stackoverflow.com/questions/21208801/
-#'     do({ezsum = 
-#'       tidyr::gather(., key, value) %>% # you can use tidyr::gather(., variable, category)
-#'       ungroup() %>%
-#'       count(key, value) %>% 
-#'       mutate(p = n/sum(n))
-#'     }) %>% 
-#'     ungroup() %>%
-#'     filter(!key %in% grp_cols) # not sure whiy appear as key a group col
-#'   
-#' }
+#' Generate Summary Tables for categorical variables
+#'
+#' @param df A data frame.
+#'
+#' @examples
+#'
+#' data("credit")
+#'
+#' credit %>% select_categorical() %>% ez_summ_cat()
+#' 
+#' @export
+ez_summ_cat <- function(df){
+
+  var <- NULL
+
+  grp_cols <- names(attr(df, "labels"))
+
+  df %>%
+    purrr::map_if(is.factor, as.character) %>% # avoid warning
+    dplyr::as_data_frame() %>% # this ungroup the df
+    dplyr::group_by_(.dots = lapply(grp_cols, as.symbol)) %>%  # http://stackoverflow.com/questions/21208801/
+    dplyr::do({ezsum =
+      tidyr::gather(., var, value) %>% 
+      dplyr::ungroup() %>%
+      dplyr::count(var, value) %>%
+      dplyr::mutate(p = n/sum(n))
+    }) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!var %in% grp_cols) # not sure whiy appear as var a group col
+
+}
+
+#' Generate Summary Tables for numercial variables
+#'
+#' @param df A data frame.
+#'
+#' @examples
+#'
+#' data("credit")
+#'
+#' credit %>% select_numeric() %>% ez_summ_num()
+#' 
+#' @export
+ez_summ_num <- function(df, na.rm = TRUE, probs = c(1:9/10)){
+  
+  r1 <- df %>%
+    dplyr::do(
+      tidyr::gather(., var, value) %>% 
+      dplyr::group_by(var) %>%
+      summarize(
+        n = length(value),
+        na = sum(is.na(value)),
+        na.percent = na/n,
+        mean = mean(value, na.rm = na.rm),
+        sd = sd(value, na.rm = na.rm),
+        min = min(value, na.rm = na.rm),
+        max = max(value, na.rm = na.rm))
+    ) %>%
+    dplyr::ungroup() 
+  
+  r2 <- df %>% 
+    dplyr::do(
+      tidyr::gather(., var, value) %>% 
+        dplyr::group_by(var) %>% 
+        do(data_frame(quantile = paste0("q", round(100*probs)),
+                      quantilevalue = quantile(.$value, probs = probs)))
+    ) %>%
+    tidyr::spread(quantile, quantilevalue)
+  
+  
+  dplyr::left_join(r1, r2, by = "var")
+  
+}
+
+    
+    
+    
+    
+    
